375a2b7745a7ed7f3aa5dcc54c37ab372bb8ae76
KRICT-DATA <60207963+KRICT-DATA@users.noreply.github.com>
2023-10-05 19:15:09 +0900

    Add files via upload

---
+++ CGCNN_searching/binary/job_files/job129.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH --job-name=Rb_GeZn_I
+#SBATCH --cpus-per-task=1        # Number of cores per MPI rank 
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=56     # How many tasks on each node
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+export OMP_NUM_THREADS=1
+
+#python random_searching_restart.py 0.cif K I Cd,Ge,Sn,Zn K_CdGeSnZn_I.pkl 1 K_CdGeSnZn_I.0.log > K_CdGeSnZn_I.1.log
+#python random_searching_restart.py 0.cif Cs Br Cd,Ge,Hg Cs_CdGeHg_Br.pkl 0 > Cs_CdGeHg_Br.log
+python ../random_searching_restart.py ../0.cif Rb I Ge,Zn Rb_GeZn_I.pkl 0 > Rb_GeZn_I.log
