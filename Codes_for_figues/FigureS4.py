7aaa2a52d10daf197f2fc2cb596500900205b01a
KRICT-DATA <60207963+KRICT-DATA@users.noreply.github.com>
2023-11-28 10:01:49 +0900

    Update FigureS4.py

--- Codes_for_figues/FigureS4.py
+++ Codes_for_figues/FigureS4.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-bandgap = 1.75
 Compounds = ['CsGe0.5625Sn0.4375Br3', 'CsGe0.3125Sn0.6875I3', 'CsPb0.3125Sn0.6875Br3', 'CsGe0.375Pb0.625I3',
              'CsGe0.0625Pb0.3125Sn0.625Br3', 'CsGe0.4375Pb0.0625Sn0.5Br3', 'CsGe0.25Pb0.5625Sn0.1875I3',
              'CsGe0.5625Hg0.3125Sn0.125Cl3', 'CsCd0.0625Ge0.25Pb0.5Sn0.1875I3', 'CsCd0.125Ge0.5625Hg0.1875Sn0.125Cl3']
@@ -10,7 +9,8 @@ compounds = [r"$\mathregular {CsGe_{0.5625}Sn_{0.4375}Br_3}$", r"$\mathregular {
              r"$\mathregular {CsGe_{0.0625}Pb_{0.3125}Sn_{0.625}Br_3}$", r"$\mathregular {CsGe_{0.4375}Pb_{0.0625}Sn_{0.5}Br_3}$",
              r"$\mathregular {CsGe_{0.25}Pb_{0.5625}Sn_{0.1875}I_3}$", r"$\mathregular {CsGe_{0.5625}Hg_{0.3125}Sn_{0.125}Cl_3}$",
              r"$\mathregular {CsCd_{0.0625}Ge_{0.25}Pb_{0.5}Sn_{0.1875}I_3}$", r"$\mathregular {CsCd_{0.125}Ge_{0.5625}Hg_{0.1875}Sn_{0.125}Cl_3}$"]
-for Compound, compound in zip(Compounds, compounds):
+bandgaps = [1.39, 1.34, 1.73, 1.77, 1.71, 1.48, 1.71, 1.75, 1.75, 1.87]
+for Compound, compound, bandgap in zip(Compounds, compounds, bandgaps):
     E, alpha = np.loadtxt(f'optical_data/{Compound}/absorption.dat', usecols=[0, 1], unpack=True)
     df = pd.read_csv(f'optical_data/{Compound}/efficiency.csv')
     thickness = np.array(df.iloc[:, 0])
